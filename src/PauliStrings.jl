5a753797c2b515c6836b92c5656e3e7afb9a930c
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-05-03 14:17:03 -0400

    comment

--- src/PauliStrings.jl
+++ src/PauliStrings.jl
@@ -6,7 +6,7 @@ export ⊗
 const ⊗ = kron
 
 function __init__()
-    # Add a convenience function if PyCall is loaded before PauliStrings.
+    # Add convenience functions if PyCall is loaded.
     # sympy can be used in any case, but this makes it more convenient
     @require PyCall="438e738f-606a-5dbb-bf0a-cddfbfd45ab0" begin
         Base.:*(z::PyCall.PyObject, ps::PauliTerm) = PauliTerm(ps.s, ps.coeff * z)
