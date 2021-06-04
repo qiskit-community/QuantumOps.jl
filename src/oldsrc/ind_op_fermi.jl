93c44074af80a888c66683f3968114d3b9d0be69
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-06-04 13:24:36 -0400

    Remove cruft in oldsrc. Saved in another branch

--- src/oldsrc/ind_op_fermi.jl
+++
@@ -1,7 +0,0 @@
-import ..FermiOps: count_raise_lower
-
-for f in (:count_raise_lower, )
-    @eval $f(op::IndOp{FermiOp}) = $f(op.op)
-end
-
-do_phase_count(f1::IndOp{<:AbstractFermiOp}, f2::IndOp{<:AbstractFermiOp}) = do_phase_count(f1.op, f2.op)
