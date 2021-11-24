a27da4b03d46734f183903a1f170057e046c52e6
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-11-24 15:08:03 -0500

    Disable example in runtests.jl because it breaks PackageCompiler

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,10 @@ include("./test_fermi.jl")
 include("./test_sparse.jl")
 include("./test_z4group.jl")
 
-@testset "quantum_ops_intro" begin
-    include("../examples/quantum_ops_intro.jl")
-end
+# DISABLE this because it confuses PackageCompiler.
+# @testset "quantum_ops_intro" begin
+#     include("../examples/quantum_ops_intro.jl")
+# end
 
 # @testset "jordan_wigner_example" begin
 #     include("../examples/jordan_wigner_example.jl")
